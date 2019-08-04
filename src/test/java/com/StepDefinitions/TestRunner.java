bb2f2a5a99b026ffcdd08a71e38575c54a1facf9
ktrinath <ktrinathkrishna@gmail.com>
2019-08-04 22:56:36 +0530

    added testruuner

---
+++ src/test/java/com/StepDefinitions/TestRunner.java
@@ -0,0 +1,20 @@
+package com.StepDefinitions;
+
+import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+
+@CucumberOptions(features="src/test/resources/Features",glue="com.StepDefinitions",dryRun=false)
+
+public class TestRunner {
+	
+	
+	
+	
+	
+	
+
+}
